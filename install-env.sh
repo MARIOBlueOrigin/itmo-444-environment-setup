2904bdd846310593e9b04a67459d9060a1bb3970
tonakiga <j.sebastien.seck@gmail.com>
2015-10-04 14:54:31 -0500

    updated install-env.sh script for correct paths hopefully

--- install-env.sh
+++ install-env.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-apt-get install -y apache2 && apt-get install -y git
+sudo apt-get update -y
+sduo apt-get install -y apache2 && apt-get install -y git
+
+git clone https://github.com/Tonakiga/itmo-444-application-setup.git
+git clone https://github.com/Tonakiga/itmo-444-images.git
+
+mv ./itmo-444-application-setup/index.html /var/www/html
+mv ./itmo-444-application-setup/page2.html /var/www/html
+mv ./itmo-444-images /var/www/images
+
 echo "Hello World!" > /tmp/hello.txt
-git clone https://github.com/Tonakiga/itmo-444-application-setup.git /var/www/html
